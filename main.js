778a33d16860e318cbe8c1e1d5ecc7c9308211ae
christy cooper betty <ccooper.prsvr@gmail.com>
2024-01-22 18:32:50 -0600

    fixed work

--- main.js
+++ main.js
@@ -1,31 +1,49 @@
-const strike = document.querySelectorAll(".ol")
-for(let i = 0;i < strike.length; i ++){
-    strike[i].addEventListener("click",()=>{
-        strike[i].style.textDecoration = "line-through"
+const list= document.querySelectorAll("ol>li")
+for(let i = 0;i < list.length; i ++){
+    list[i].addEventListener("click",()=>{
+        list[i].style.textDecoration = "line-through"
     })
 }
-const opaque = document.querySelectorAll(".ul")
+const opaque = document.querySelectorAll("ul li")
 for(let i = 0; i < opaque.length; i++){
     opaque[i].addEventListener("click",()=>{
-        opaque[i].style.opacity = "35%"
+        opaque[i].style.opacity = "15%"
     })    
 }
 
-const image = document.querySelectorAll(".pic")
-
-for(let i = 0; i < image.length; i++){
-    image[i].addEventListener("click",()=>{
-        image[i].style.opacity = "15%"
+const thirdList= document.querySelectorAll(".pic")
+for(let i = 0; i < thirdList.length; i++){
+    thirdList[i].addEventListener("click",()=>{
+        thirdList[i].style.width = 0
     })    
 }
-document.querySelector("#destroy-all").addEventListener("click",()=>{
-    for(let i = 0; i < strike.length; i++){
-        strike[i].style.textDecoration = "line-through"
-    }
+
+const button = document.querySelector("#destroy-all")
+button.addEventListener("click",()=>{
+    // for(let i = 0; i < list.length; i++){
+    //     list[i].style.textDecoration = "line-through"
+    // }
+    list.forEach((element) => {
+        element.style.textDecoration = 'line-through'
+    })
+
+
+
+
+
+
     for(let i = 0;i < opaque.length;i++){
-        opaque[i].style.opacity = ".35%"
+        opaque[i].style.opacity = ".15%"
     }
-    for(let i = 0;i < image.length; i++){
-        image[i].style.opacity = "15%"
+    for(let i = 0;i < thirdList.length; i++){
+        thirdList[i].style.opacity = 0
     }
 })
+
+
+
+
+
+
+
+
